1c102152b73b3909ddecc26caa1bfddb8272aec9
yogevye <yogevye1@gmail.com>
2020-05-10 08:46:25 +0300

    ussr auth exports

--- common/auth/src/index.js
+++ common/auth/src/index.js
@@ -1,2 +1,4 @@
 const UserAuthRouter = require('./user-auth-router');
 const UserAuthMiddleware = require('./middleware/user-auth-middleware');
+
+module.exports = {UserAuthRouter, UserAuthMiddleware};
